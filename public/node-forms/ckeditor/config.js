85c53c4b39c32c47cc69f3fc23a9de673006055f
etai <etai@empeeric.com>
2013-01-28 20:59:57 +0200

    editor

--- public/node-forms/ckeditor/config.js
+++ public/node-forms/ckeditor/config.js
@@ -1,17 +1,17 @@
 ﻿/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
-	// Define changes to default configuration here. For example:
-	config.language = 'en';
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    config.language = 'en';
     config.forcePasteAsPlainText = true;
-	// config.uiColor = '#AADC6E';
-	config.toolbar = [
-   ['Source'],['Bold','Italic','Underline','StrikeThrough','-','Outdent','Indent','-','Blockquote'],
-   ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-   ['Link']
-	] ;
+    // config.uiColor = '#AADC6E';
+    config.toolbar = [
+        ['Source'],
+        ['Bold', 'Italic', 'Underline', 'StrikeThrough', '-', 'Outdent', 'Indent', '-', 'Blockquote'],
+        ['NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+        ['Link'], ['Styles', 'Format', 'Font', 'FontSize']
+    ];
 };
