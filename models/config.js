a14d18f6b3eddd8c2c79b0bd83693f529e824fd9
etai <etai@empeeric.com>
2013-02-12 18:54:16 +0200

    facebook link

--- models/config.js
+++ models/config.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose'),
 var schema = new mongoose.Schema({
     title: { type: String, required: true },
     email: { type: String, required: true },
-    contact_details: { type: Types.Html }
+    contact_details: { type: Types.Html },
+    facebook_text: { type: String, default: 'Our Facebook' }
 });
 
 var config = module.exports = mongoose.model('config', schema);
